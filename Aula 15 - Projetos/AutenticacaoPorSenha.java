f8bced88f9ec8e86c8818a1be127c45dc20257f9
Melissa <39806633+Melissa-Lima@users.noreply.github.com>
2018-07-27 09:46:32 -0300

    Update AutenticacaoPorSenha.java

--- Aula 15 - Projetos/AutenticacaoPorSenha.java
+++ Aula 15 - Projetos/AutenticacaoPorSenha.java
@@ -1,1 +1,22 @@
+package autenticacao;
+import java.util.scanner;
 
+public class AutenticacaoPorSenha implements IMetodoAutenticacao{
+	
+	
+	@Override
+	public UsuarioAutenticado autenticar() throws UsuarioNaoAutenticadoException{
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Digite o codigo");
+		int codigo = leitor.nextInt();
+		System.out.println("Digite o nome");
+		String nome = leitor.next();
+		System.out.println("Digite a senha");
+		String nome = leitor.next();
+		
+		if (!senha.equals("1234"))
+			throw new UsuarioNaoAutenticadoException{
+				
+			}
+	}
+}
