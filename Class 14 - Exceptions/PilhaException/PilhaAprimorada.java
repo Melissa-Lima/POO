bcdc2ddeb8199a1ffb40fc5e17c6ed38fe5e3b2f
Melissa <39806633+Melissa-Lima@users.noreply.github.com>
2018-08-07 16:42:34 -0300

    Update PilhaAprimorada.java

--- Class 14 - Exceptions/PilhaException/PilhaAprimorada.java
+++ Class 14 - Exceptions/PilhaException/PilhaAprimorada.java
@@ -1,28 +1,29 @@
+package estrutura;
+
 public class PilhaAprimorada extends PilhaSimples{
-		
-	private int tamanho;
 	
-	public PilhaAprimorada(int tamanho){
-		super(tamanho);
-	}
-		
-	@Override
-	public void empilha(Object novoItem) throws PilhaCheiaException {
-		if (getTopo()==super.getTamanhoMax()-1)
-			throw new PilhaCheiaException(this.getTamanhoMax(),novoItem);
-		else
-			super.empilha(novoItem);
+	public PilhaAprimorada(int tamanhoMax){
+		super(tamanhoMax);
 	}
 	
 	@Override
-	public Object desempilha() throws PilhaCheiaException {
-		if (super.getTopo()==-1){
-			throw new PilhaCheiaException(super.getTopo());
+	public void empilha(Object item){
+		if (this.getTopo()==getTamanhoMax()-1){
+			throw new PilhaCheiaException(item, getTamanhoMax());
+		} 
+		
+		else{
+			super.empilha(item);
+		}
+	}
+
+	@Override
+	public Object desempilha(){
+		if(this.getTopo()==-1){
+			throw new PilhaVaziaException();
 		}
 		else{
 			return(super.desempilha());
-			
 		}
 	}
 }
-
