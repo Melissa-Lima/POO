db9b052e33ee7051a7102bd4a828e7a01d7d5949
Melissa <39806633+Melissa-Lima@users.noreply.github.com>
2018-08-07 16:43:05 -0300

    Update PilhaCheiaException.java

--- Class 14 - Exceptions/PilhaException/PilhaCheiaException.java
+++ Class 14 - Exceptions/PilhaException/PilhaCheiaException.java
@@ -1,21 +1,5 @@
-public class PilhaCheiaException extends RunTimeException{
-  
-  	int erro;
-  
-   	public PilhaCheiaException(int tamanho, Object novoItem){
-     	erro=0;
-   	}
-   	public PilhaCheiaException(int topo){
-   		 erro=1;    
-   	}
-     
-@Override
-	public String getMessage(){
-  		if (erro==0){
-    		return("Pilha Cheia!");
-  		}
-  		else{
-   		 	return("Já esvaziou toda a pilha.");
-		}
-	}
+package estrutura;
+
+public class PilhaException extends RuntimeException{
+	
 }
